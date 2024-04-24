138a66dc56b1b19791e54480bf16bbb2e2ed0775
Jacob Burroughs <jburroughs@instructure.com>
2024-04-24 10:56:15 -0500

    Skip another rubygems-update spec

--- debian/tests/excludes/any/TestGemCommandsUpdateCommand.rb
+++ debian/tests/excludes/any/TestGemCommandsUpdateCommand.rb
@@ -9,5 +9,6 @@ exclude :test_execute_system_specific_newer_than_or_equal_to_3_2_leaves_plugins_
 exclude :test_execute_system_specific_older_than_3_2_removes_plugins_dir, "gem update --system was disabled by rubygems-integration"
 exclude :test_execute_system_specifically_to_latest_version, "gem update --system was disabled by rubygems-integration"
 exclude :test_execute_system_when_latest_does_not_support_your_ruby, "gem update --system was disabled by rubygems-integration"
+exclude :test_execute_system_when_latest_does_not_support_your_ruby_but_previous_one_does, "gem update --system was disabled by rubygems-integration"
 exclude :test_execute_system_update_installed, "gem update --system was disabled by rubygems-integration"
 exclude :test_execute_system_update_installed_in_non_default_gem_path, "gem update --system was disabled by rubygems-integration"
