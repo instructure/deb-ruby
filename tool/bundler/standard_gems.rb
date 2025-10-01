0f4ebeb6f4fac6f465349a36274f1d8c1d1ecf70
Aaron Ogata <aogata@instructure.com>
2025-10-01 12:34:12 -0700

    New upstream version 3.1.7

---
+++ tool/bundler/standard_gems.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "standard", "1.12.1"
+gem "parser", "3.2.2.2"
+
+gem "minitest"
+gem "rake"
+gem "rake-compiler"
+gem "rspec"
+gem "test-unit"
